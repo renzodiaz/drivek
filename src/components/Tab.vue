4b27e4e466e8bd8effaaf200a4561deb3a89f963
Renzo Diaz <dev.renzo.diaz@gmail.com>
2018-06-11 17:04:51 -0500

    basic styles added.

---
+++ src/components/Tab.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+<script>
+export default {
+    name: Tab,
+    data() {
+        return {}
+    }
+}
+</script>
--- src/components/shared/Slider.vue
+++ src/components/shared/Slider.vue
@@ -16,6 +16,36 @@
                     </div>
                 </div>
             </div>
+            <div class="slider__item">
+                <div class="container">
+                    <div class="item__hero">
+                        <h2>Contest</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid commodi cum dolore eveniet
+                        in ipsa laudantium magnam mollitia natus nihil nulla optio quam quas rerum suscipit, tenetur ullam
+                        voluptates.
+                        <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid commodi cum
+                        dolore eveniet in ipsa laudantium magnam mollitia natus nihil nulla optio quam quas rerum
+                        suscipit, tenetur ullam voluptates.</p>
+                        <a href="" class="button">Gallery Showcase</a>
+                        <a href="" class="button is--yellow">Participate</a>
+                    </div>
+                </div>
+            </div>
+            <div class="slider__item">
+                <div class="container">
+                    <div class="item__hero">
+                        <h2>Contest</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid commodi cum dolore eveniet
+                        in ipsa laudantium magnam mollitia natus nihil nulla optio quam quas rerum suscipit, tenetur ullam
+                        voluptates.
+                        <br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid commodi cum
+                        dolore eveniet in ipsa laudantium magnam mollitia natus nihil nulla optio quam quas rerum
+                        suscipit, tenetur ullam voluptates.</p>
+                        <a href="" class="button">Gallery Showcase</a>
+                        <a href="" class="button is--yellow">Participate</a>
+                    </div>
+                </div>
+            </div>
         </slick>
     </div>
 </template>
